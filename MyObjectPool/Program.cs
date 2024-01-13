1391e0d0d220b4e260abc575239b18bf3317ca28
Iasonas <iasonas.ps@gmail.com>
2024-01-13 10:33:16 +0200

    Added some comments

--- MyObjectPool/Program.cs
+++ MyObjectPool/Program.cs
@@ -11,10 +11,16 @@
             Student student = objectPool.getObject();
 
             Console.WriteLine("First object assigned.Lets check!");
+            
+            Console.WriteLine("Objects number in object pool must now be equal to 0 : [{0}]", objectPool.getObjectsNUmber());
+
+            Console.WriteLine("Now we release the object and ...");
+
+            Thread.Sleep(1000);
 
             objectPool.releaseObject(student);
 
-            Console.WriteLine("Objects number must now be equal to 1 : [{0}]",objectPool.getObjectsNUmber());
+            Console.WriteLine("Objects number in object pool must now be equal to 1 : [{0}]",objectPool.getObjectsNUmber());
 
             Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - - - - - - - - ");
         }
